d0b16ee4c3f3c3c25c2b3f9f040637c275c1cbfc
Caio <bukvar2009@gmail.com>
2023-08-03 12:58:13 -0300

    adicionando funcionalidade para removeProduct do array

--- learning-angular/src/app/components/product-list/product-list.component.ts
+++ learning-angular/src/app/components/product-list/product-list.component.ts
@@ -9,4 +9,19 @@ export class ProductListComponent {
   @Input() name!: string;
   @Input() amount!: number;
   @Input() products!: Array<{ productName: string, productAmount: number }>
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  removeProduct(productName: string) {
+    for (let index = 0; index < this.products.length; index++) {
+      if (this.products[index].productName === productName) {
+        this.products.splice(index, 1);
+        break;
+      }
+    }
+
+    return this.products;
+  }
 }
