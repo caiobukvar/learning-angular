2926c8a185ecb62ebbdb84fd63a89710d4e6086d
Caio <bukvar2009@gmail.com>
2023-08-03 10:48:30 -0300

    adicionando rotas para o routing modules

--- learning-angular/src/app/app-routing.module.ts
+++ learning-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  // { path: 'form-test', component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
