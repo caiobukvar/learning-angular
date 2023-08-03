bc9c5e2132c83eccfbeb4c465ca7808c9de3e354
Caio <bukvar2009@gmail.com>
2023-08-02 23:12:07 -0300

    removendo componente navbar

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     MainPageComponent
   ],
   imports: [
