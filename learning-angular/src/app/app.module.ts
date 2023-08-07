39ad9e1d04aa7387acb14765d6ede3da88a3b882
Caio <bukvar2009@gmail.com>
2023-08-07 11:03:41 -0300

    removing all angular material but divider

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -1,18 +1,19 @@
+import { DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { MatCommonModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DatePipe } from '@angular/common';
-import { MainPageComponent } from './components/main-page/main-page.component';
 import { FormTestComponent } from './components/form-test/form-test.component';
+import { GithubUserSearchComponent } from './components/github-user-search/github-user-search.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { GithubUserSearchComponent } from './components/github-user-search/github-user-search.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialComponent } from './components/angular-material/angular-material.component';
-import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,6 @@ import { MatDividerModule } from '@angular/material/divider';
     PersonalInfoComponent,
     ProductListComponent,
     GithubUserSearchComponent,
-    AngularMaterialComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +30,10 @@ import { MatDividerModule } from '@angular/material/divider';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatDividerModule
+
+    MatCommonModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [
     DatePipe
